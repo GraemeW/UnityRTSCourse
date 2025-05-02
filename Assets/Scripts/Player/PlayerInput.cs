d1cb5846fb4476d2b75e77358dc24ccafc17b1a4
Graeme Williams <graeme.m.williams@gmail.com>
2025-05-02 12:09:05 -0400

    Add camera zoom in/out

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -6,10 +7,35 @@ namespace GameDevTV.RTS
 {
     public class PlayerInput : MonoBehaviour
     {
+        // Tunables
         [SerializeField] private Transform cameraTarget;
+        [SerializeField] private CinemachineCamera cinemachineCamera;
         [SerializeField] private float keyboardPanSpeed = 5;
+        [SerializeField] private float minZoomDistance = 5;
+        [SerializeField] private float zoomSpeed = 1;
+
+        // Cached References
+        private CinemachineFollow cinemachineFollow;
+
+        // State
+        private float zoomStartTime;
+        private Vector3 startingFollowOffset;
+
+        private void Awake()
+        {
+            cinemachineFollow = cinemachineCamera.GetComponent<CinemachineFollow>();
+            startingFollowOffset = cinemachineFollow.FollowOffset;
+        }
 
         private void Update()
+        {
+            if (cinemachineFollow == null) { return; }
+
+            HandlePanning();
+            HandleZooming();
+        }
+
+        private void HandlePanning()
         {
             Vector3 moveAmount = Vector2.zero;
 
@@ -21,6 +47,45 @@ namespace GameDevTV.RTS
             moveAmount *= Time.deltaTime;
 
             cameraTarget.position = cameraTarget.position + moveAmount;
+        }
+
+        private void HandleZooming()
+        {
+            if (ShouldSetZoomStartTime())
+            {
+                zoomStartTime = Time.time;
+            }
+
+            Vector3 targetFollowOffset;
+            float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+
+            if (Keyboard.current.endKey.isPressed)
+            {
+                targetFollowOffset = new Vector3(
+                    cinemachineFollow.FollowOffset.x,
+                    minZoomDistance,
+                    cinemachineFollow.FollowOffset.z
+                );
+            }
+            else
+            {
+                targetFollowOffset = new Vector3(
+                    cinemachineFollow.FollowOffset.x,
+                    startingFollowOffset.y,
+                    cinemachineFollow.FollowOffset.z
+                );
+            }
+
+            cinemachineFollow.FollowOffset = Vector3.Slerp(
+                cinemachineFollow.FollowOffset,
+                targetFollowOffset,
+                zoomTime
+            );
+        }
+
+        private static bool ShouldSetZoomStartTime()
+        {
+            return Keyboard.current.endKey.wasPressedThisFrame || Keyboard.current.endKey.wasReleasedThisFrame;
         }
     }
 }
